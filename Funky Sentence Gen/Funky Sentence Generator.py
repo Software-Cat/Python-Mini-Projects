c5af758d458362aaffab17e859f4c0126a348e7f
Bowen Wu <51037053+Software-Cat@users.noreply.github.com>
2020-07-17 19:10:25 +1000

    Add files via upload

---
+++ Funky Sentence Gen/Funky Sentence Generator.py
@@ -0,0 +1,19 @@
+import random
+adjectives = ["abandoned","baffled","cringy","dazzling","eccentric","fancy","generous","happy","ill","jocose","kind","lazy","magical","naked",
+              "obstinate","patriotic","queasy","raging","savage","talented","unlucky","vegetarian","white","xenophobic","yawning","zippy"]
+
+executerNouns = ["apple","banana","cat","dog","elephatnt","flamingo","giraffe","hippo","iguana","jellyfish","kangaroo","ladybug","mammoth","numbat",
+                 "octopus","panda","quail","rabbit","snake","teacher","umpire","vocalist","whale","xylophone","yoga instructor","zoologist"]
+
+adverbs = ["accidentally","beneficially","chaotically","doubtfully","efficiently","fearfullly","gently","hypocritically","impulsively","jealously","keenly",
+           "loudly","mysteriously","naively","obediently","passionately","quietly","rationally","sadly","telepathically","uncontrollably","viciously",
+           "wildly","xenophobically","youthfully","zealously"]
+
+verbs = ["ate", "bent", "cleaned", "danced", "educated", "fabricated", "grew" , "hacked", "immobilized"]
+genders = ["his","hers"]
+subjectNouns = ["aquarium", "bandana", "cabbage"]
+prepositions = ["with","without","in front of","behind","next to","under","over"]
+objects = ["aeroplane", "broom"]
+
+print("The " + random.choice(adjectives) + " " + random.choice(executerNouns) + " " + random.choice(adverbs) + " " + random.choice(verbs) + " " + \
+      random.choice(genders) + " " + random.choice(subjectNouns) + " " + random.choice(prepositions) + " a/an " + random.choice(objects) + ".")
